552820779b4c63e484cf53da81c5887925ba6ce6
Gerardo Saca <gsacavdm@hotmail.com>
2019-03-10 13:24:31 -0700

    Switch to storage environment variables

--- linux-cse/examples/upload-storage.sh
+++ linux-cse/examples/upload-storage.sh
@@ -13,8 +13,8 @@ echo "Loading azure-cse-utils"
 . ./azure-cse-utils.sh
 
 echo "Looking up blob $storage_account_name..."
-resource_group=$(az storage account list --query "[?name=='$storage_account_name'].resourceGroup" -o json | jq -r '.[0]')
-if [ "$resource_group" == "null" ]
+storage_account=$(az storage account list --query "[?name=='$storage_account_name'].name" -o json | jq -r '.[0]')
+if [ "$storage_account" == "null" ]
 then
   resource_group=$(getAzureResourceGroup)
 
@@ -23,17 +23,21 @@ then
 fi
 
 echo "Getting storage account $storage_account_name key..."
-storage_account_key=$(az storage account keys list -g $resource_group -n $storage_account_name --query "[0].value" -o tsv)
+storage_account_key=$(az storage account keys list -n $storage_account_name --query "[0].value" -o tsv)
+
+echo "Setting storage environment variables"
+export AZURE_STORAGE_ACCOUNT=$storage_account_name
+export AZURE_STORAGE_KEY=$storage_account_key
 
 echo "Looking up container $storage_container_name in storage account $storage_account_name..."
-storage_container=$(az storage container list --account-name $storage_account_name --account-key $storage_account_key --query "[?name=='$storage_container_name'].name" -o json | jq -r '.[0]')
+storage_container=$(az storage container list --query "[?name=='$storage_container_name'].name" -o json | jq -r '.[0]')
 if [ "$storage_container" == "null" ]
 then
   echo "Container $storage_container_name in storage account $storage_account_name not found, creating..."
-  az storage container create -n $storage_container_name --account-name $storage_account_name --account-key $storage_account_key
+  az storage container create -n $storage_container_name
 fi
 
 echo "Uploading file $file to storage account $storage_account_name, container $storage_container_name..."
-az storage blob upload -n $(basename $file) -c $storage_container_name -f $file --account-name $storage_account_name --account-key $storage_account_key
+az storage blob upload -n $(basename $file) -c $storage_container_name -f $file
 
 echo "Done!"
