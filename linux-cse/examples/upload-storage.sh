ddfe188197948ca32d0af9b63de1aa1b287d6d8f
Gerardo Saca <gsacavdm@hotmail.com>
2019-03-10 12:12:41 -0700

    Get upload-storage working

--- linux-cse/examples/upload-storage.sh
+++ linux-cse/examples/upload-storage.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 
-delete_after_done=$0
 execution_id=$1
 
 # Replace with your own parameters:
 file=$2
 storage_account_name=$3
+storage_container_name=$4
 
 set -e
 
 echo "Loading azure-cse-utils"
 . ./azure-cse-utils.sh
 
-echo "Running custom script"
-metadata=$(curl "http://169.254.169.254/metadata/instance/compute?api-version=2018-02-01" -H "Metadata:true")
-resource_group_name=$(echo $metadata | jq .resourceGroupName -r)
-location=$(echo $metadata | jq .location -r)
+echo "Looking up blob $storage_account_name..."
+resouce_group=$(az storage account list --query "[?name=='$storage_account_name'].resourceGroup" -o json | jq -r '.[0]')
+if [ "$resouce_group" == "null" ]
+then
+  resource_group=$(getAzureResourceGroup)
 
-storage_account=$(az storage account list | grep -w $storage_account_name)
-
-if [ ! -z $storage_account ]; then
-  az storage account create -g $resource_group_name -n  $storage_account_name -l $location --sku Standard_LRS --kind Storage
+  echo "Storage account $storage_account_name not found, creating in resource group $resource_group..."
+  az storage account create -g $resource_group -n $storage_account_name
 fi
+
+echo "Getting storage account $storage_account_name key..."
+storage_account_key=(az storage account keys list -g $resource_group -n $storage_account_name --query "[0].value" -o tsv)
+
+echo "Looking up container $storage_container_name in storage account $storage_account_name..."
+storage_container=$(az storage container list --account-name $storage_account_name --account-key $storage_account_key --query "[?name=='$storage_container_name'].name" -o json | jq -r '.[0]')
+if [ "$storage_container" == "null" ]
+then
+  echo "Container $storage_container_name in storage account $storage_account_name not found, creating..."
+  az storage container create -n $storage_container_name --account-name $storage_account_name --account-key $storage_account_key
+fi
+
+echo "Uploading file $file to storage account $storage_account_name, container $storage_container_name..."
+az storage blob upload -n $(basename $file) -c $storage_container_name -f $file --account-name $storage_account_name --account-key $storage_account_key
+
+echo "Done!"
